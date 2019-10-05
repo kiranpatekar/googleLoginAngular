5b1f61b534328cb8b89ea6b6613950d707f89a11
kiranPatekar <kiran.patekar3@gmail.com>
2019-10-05 18:41:47 +0530

    firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { FormsModule } from '@angular/forms';
+
+
+ // Your web app's Firebase configuration
+ var config = {
+  apiKey: "AIzaSyADniKYA7nsvFvMlDURTxYOoJQAesOb49s",
+  authDomain: "shifa-8bfcd.firebaseapp.com",
+  databaseURL: "https://shifa-8bfcd.firebaseio.com",
+  projectId: "shifa-8bfcd",
+  storageBucket: "",
+  messagingSenderId: "135858216461",
+  appId: "1:135858216461:web:70b7b0429f5faacabe3f44"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
